5df91f91a7ea84a231c8a9a2e7ebc49bc957d0b4
Serg <serg@example.com>
2021-11-16 00:46:03 +0300

    added additional info in component

--- 00-basics/src/components/card/card.component.jsx
+++ 00-basics/src/components/card/card.component.jsx
@@ -2,6 +2,11 @@ import React from 'react';
 
 export const Card = props => (
   <div>
+  <img
+    alt='monster'
+    src={`https://robohash.org/${props.monster.id}?set=set2&size=180x180`}
+  />
     <h2> {props.monster.name} </h2>
+    <p> {props.monster.email} </p>
   </div>
 );
