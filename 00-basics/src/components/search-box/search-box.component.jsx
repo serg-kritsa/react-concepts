2c8dd0be355c82d04ebba083ef6fd78fe1f86e28
Serg <serg@example.com>
2021-11-16 01:00:25 +0300

    added styles to search-box component

--- 00-basics/src/components/search-box/search-box.component.jsx
+++ 00-basics/src/components/search-box/search-box.component.jsx
@@ -1,7 +1,10 @@
 import React from 'react';
 
+import './search-box.styles.css';
+
 export const SearchBox = props => (
   <input
+    className='search-box'
     type='text'
     placeholder='search monsters'
     onChange={props.onSearchChange}
