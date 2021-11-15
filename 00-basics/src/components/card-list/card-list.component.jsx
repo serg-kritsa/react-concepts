7330400f9d247c98951d66b3e9cf5b3eb6cbd8c0
Serg <serg@example.com>
2021-11-16 00:41:26 +0300

    added css grid

--- 00-basics/src/components/card-list/card-list.component.jsx
+++ 00-basics/src/components/card-list/card-list.component.jsx
@@ -2,8 +2,10 @@ import React from 'react';
 
 import { Card } from '../card/card.component';
 
+import './card-list.styles.css';
+
 export const CardList = props => (
-  <div>
+  <div className='card-list'>
   {props.monsters.map(monster => (
     <Card key={monster.id} monster={monster} />
   ))}
