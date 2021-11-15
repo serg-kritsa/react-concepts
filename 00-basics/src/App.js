ce4a822afd333e01c44dc49af12b58b4ca9fca47
Serg <serg@example.com>
2021-11-15 22:59:23 +0300

    print data from array

--- 00-basics/src/App.js
+++ 00-basics/src/App.js
@@ -2,15 +2,20 @@ import React, { Component } from 'react';
 
 class App extends Component {
   title = 'Hello React';
+  users = [{name: 'user1'}, {name: 'user2'}];
 
   render() {
     const title = this.title;
     return (
       <div>
         <h1>{title}</h1>
+        {this.users.map(u => (
+          <p>{u.name}</p>
+        ))}
       </div>
     );
   }
 }
 
 export default App;
+
