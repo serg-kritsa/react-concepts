931f3f721fdee541515752c1ae75bd35cd161280
Serg <serg@example.com>
2021-11-15 22:54:06 +0300

    string interpolation

--- 00-basics/src/App.js
+++ 00-basics/src/App.js
@@ -1,10 +1,13 @@
 import React, { Component } from 'react';
 
 class App extends Component {
+  title = 'Hello React';
+
   render() {
+    const title = this.title;
     return (
       <div>
-        <h1>Hello React</h1>
+        <h1>{title}</h1>
       </div>
     );
   }
