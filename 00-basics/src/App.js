7896f516fc2a8e27b29a92ffedb5a1bbb700c89e
Serg <serg@example.com>
2021-11-15 23:01:20 +0300

    added constructor w/ state

--- 00-basics/src/App.js
+++ 00-basics/src/App.js
@@ -1,6 +1,13 @@
 import React, { Component } from 'react';
 
 class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      
+    };
+  }
   title = 'Hello React';
   users = [{name: 'user1'}, {name: 'user2'}];
 
