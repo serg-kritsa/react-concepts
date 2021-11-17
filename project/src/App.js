fbdbaffbcbb5022f9ae3de2361dc81de5ad98c17
Serg <serg@example.com>
2021-11-17 23:47:36 +0300

    added '/hats' route

--- project/src/App.js
+++ project/src/App.js
@@ -5,11 +5,18 @@ import './App.css';
 
 import HomePage from './pages/homepage/homepage.component';
 
+const HatsPage = () => (
+  <div>
+    <h1>HATS PAGE </h1>
+  </div>
+);
+
 function App() {
   return (
     <div>
       <Switch>
         <Route exact path='/' component={HomePage} />
+        <Route path='/hats' component={HatsPage} />
       </Switch>
     </div>
   );
