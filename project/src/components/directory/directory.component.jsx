bfa14e16f37095771d61fe697abf244c44845ea2
Serg <serg@example.com>
2021-11-17 23:18:07 +0300

    passed props into one more component

--- project/src/components/directory/directory.component.jsx
+++ project/src/components/directory/directory.component.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 
+import MenuItem from '../menu-item/menu-item.component';
+
 import './directory.styles.scss';
 
 class Directory extends React.Component {
@@ -43,7 +45,7 @@ class Directory extends React.Component {
     return (
       <div className='directory-menu'>
         {this.state.sections.map(({ title, imageUrl, id, size }) => (
-          <div> {id} {title} {imageUrl} {size} </div>
+          <MenuItem key={id} title={title} imageUrl={imageUrl} size={size} />
         ))}
       </div>
     );
