d56c4a5a1db01efeb6c913a219005a81ea9eead8
Serg <serg@example.com>
2021-11-17 22:50:47 +0300

    home component w/ scss

--- project/src/App.js
+++ project/src/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
 
+import HomePage from './homepage.component';
+
 import './App.css';
 
 class App extends React.Component {
   render() {
     return (
       <div className='App'>
+        <HomePage />
       </div>
     );
   }
