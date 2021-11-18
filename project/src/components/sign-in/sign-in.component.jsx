db9db37dc4a94035443e285881968d55d0b4aefa
Serg <serg@example.com>
2021-11-18 23:04:12 +0300

    added inputs as separate component

--- project/src/components/sign-in/sign-in.component.jsx
+++ project/src/components/sign-in/sign-in.component.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 
+import FormInput from '../form-input/form-input.component';
 import CustomButton from '../custom-button/custom-button.component';
 
 import './sign-in.styles.scss';
@@ -20,6 +21,12 @@ class SignIn extends React.Component {
     this.setState({ email: '', password: '' });
   };
 
+  handleChange = event => {
+    const { value, name } = event.target;
+
+    this.setState({ [name]: value });
+  };
+
   render() {
     return (
       <div className='sign-in'>
@@ -27,6 +34,22 @@ class SignIn extends React.Component {
         <span>Sign in with your email and password</span>
 
         <form onSubmit={this.handleSubmit}>
+          <FormInput
+            name='email'
+            type='email'
+            handleChange={this.handleChange}
+            value={this.state.email}
+            label='email'
+            required
+          />
+          <FormInput
+            name='password'
+            type='password'
+            value={this.state.password}
+            handleChange={this.handleChange}
+            label='password'
+            required
+          />
           <CustomButton type='submit'> Sign in </CustomButton>
         </form>
       </div>
