828f5801e81f31c5aa919505c0e721745f40d011
Serg <serg@example.com>
2021-11-20 01:18:49 +0300

    REDUX: added empty reducer for training

---
+++ project/src/redux/cart/cart.reducer.js
@@ -0,0 +1,12 @@
+const INITIAL_STATE = {
+  hidden: true
+};
+
+const cartReducer = (state = INITIAL_STATE, action) => {
+  switch (action.type) {
+    default:
+      return state;
+  }
+};
+
+export default cartReducer;
---
+++ project/src/redux/cart/cart.types.js
@@ -0,0 +1,4 @@
+const CartActionTypes = {
+};
+
+export default CartActionTypes;
