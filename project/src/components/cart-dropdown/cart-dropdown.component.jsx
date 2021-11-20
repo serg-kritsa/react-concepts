ae8e8ef1e0247fc59ad33a26ab448fcfe6827310
Serg <serg@example.com>
2021-11-20 18:20:11 +0300

    REDUX: dispatch action shorthand

--- project/src/components/cart-dropdown/cart-dropdown.component.jsx
+++ project/src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -6,10 +6,11 @@ import { withRouter } from 'react-router-dom';
 import CustomButton from '../custom-button/custom-button.component';
 import CartItem from '../cart-item/cart-item.component';
 import { selectCartItems } from '../../redux/cart/cart.selectors';
+import { toggleCartHidden } from '../../redux/cart/cart.actions.js';
 
 import './cart-dropdown.styles.scss';
 
-const CartDropdown = ({ cartItems, history }) => (
+const CartDropdown = ({ cartItems, history, dispatch }) => (
   <div className='cart-dropdown'>
     <div className='cart-items'>
       {cartItems.length ? (
@@ -23,6 +24,7 @@ const CartDropdown = ({ cartItems, history }) => (
     <CustomButton
       onClick={() => {
         history.push('/checkout');
+        dispatch(toggleCartHidden());
       }}
     >
       GO TO CHECKOUT
