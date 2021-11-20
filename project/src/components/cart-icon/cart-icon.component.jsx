97c98822533a6553cdb12ba490207b24d665af46
Serg <serg@example.com>
2021-11-20 17:34:43 +0300

    REDUX: reselect from npm

--- project/src/components/cart-icon/cart-icon.component.jsx
+++ project/src/components/cart-icon/cart-icon.component.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { connect } from 'react-redux';
 
 import { toggleCartHidden } from '../../redux/cart/cart.actions';
+import { selectCartItemsCount } from '../../redux/cart/cart.selectors';
 
 import { ReactComponent as ShoppingIcon } from '../../assets/shopping-bag.svg';
 
@@ -18,12 +19,9 @@ const mapDispatchToProps = dispatch => ({
   toggleCartHidden: () => dispatch(toggleCartHidden())
 });
 
-const mapStateToProps = ({ cart: { cartItems }}) => {
-  console.log('DOWNSIDE: called on action of OTHER combined-in-store reducer');
-  return ({
-    itemCount: cartItems.reduce((total, elem) => total + elem.quantity, 0)
-  });
-};
+const mapStateToProps = state => ({
+  itemCount: selectCartItemsCount(state)
+});
 
 export default connect(
   mapStateToProps,
