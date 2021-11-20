10d64e26582d8b3484b5c8089e1d5756a2aecdf6
Serg <serg@example.com>
2021-11-20 16:22:57 +0300

    REDUX: dispatching acton to store

--- project/src/components/collection-item/collection-item.component.jsx
+++ project/src/components/collection-item/collection-item.component.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
+import { connect } from 'react-redux';
 
 import CustomButton from '../custom-button/custom-button.component';
+import { addItem } from '../../redux/cart/cart.actions';
 
 import './collection-item.styles.scss';
 
-const CollectionItem = ({ item }) => {
+const CollectionItem = ({ item, addItem }) => {
   const { name, price, imageUrl } = item;
 
   return (
@@ -19,11 +21,18 @@ const CollectionItem = ({ item }) => {
         <span className='name'>{name}</span>
         <span className='price'>{price}</span>
       </div>
-      <CustomButton onClick={() => {}} inverted>
+      <CustomButton onClick={() => addItem(item)} inverted>
         Add to cart
       </CustomButton>
     </div>
   );
 };
 
-export default CollectionItem;
+const mapDispatchToProps = dispatch => ({
+  addItem: item => dispatch(addItem(item))
+});
+
+export default connect(
+  null,
+  mapDispatchToProps
+)(CollectionItem);
