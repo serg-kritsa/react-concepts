e53c77c9748df7c815c78302a846d5dfb8fccb00
Serg <serg@example.com>
2021-11-20 18:47:48 +0300

    REDUX: dispatch added action

--- project/src/components/checkout-item/checkout-item.component.jsx
+++ project/src/components/checkout-item/checkout-item.component.jsx
@@ -1,8 +1,11 @@
 import React from 'react';
+import { connect } from 'react-redux';
+
+import { clearItemFromCart } from '../../redux/cart/cart.actions';
 
 import './checkout-item.styles.scss';
 
-const CheckoutItem = ({ cartItem }) => {
+const CheckoutItem = ({ cartItem, clearItem }) => {
   const { name, imageUrl, price, quantity } = cartItem;
   return (
     <div className='checkout-item'>
@@ -12,11 +15,18 @@ const CheckoutItem = ({ cartItem }) => {
       <span className='name'>{name}</span>
       <span className='quantity'>{quantity}</span>
       <span className='price'>{price}</span>
-      <div className='remove-button'>
+      <div className='remove-button' onClick={() => clearItem(cartItem)}>
         &#10005;
       </div>
     </div>
   );
 };
 
-export default CheckoutItem;
+const mapDispatchToProps = dispatch => ({
+  clearItem: item => dispatch(clearItemFromCart(item))
+});
+
+export default connect(
+  null,
+  mapDispatchToProps
+)(CheckoutItem);
