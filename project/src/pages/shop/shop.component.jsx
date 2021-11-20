dc111fc17e1e797cf6cd9e5ee3bad965fc7009e3
Serg <serg@example.com>
2021-11-20 23:28:22 +0300

    moved shop data to store

--- project/src/pages/shop/shop.component.jsx
+++ project/src/pages/shop/shop.component.jsx
@@ -1,28 +1,22 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import { createStructuredSelector } from 'reselect';
 
-import SHOP_DATA from './shop.data.js';
+import { selectCollectionsForPreview } from '../../redux/shop/shop.selectors';
 
-import CollectionPreview from '../../components/collection-preview/collection-preview';
 
-class ShopPage extends React.Component {
-  constructor(props) {
-    super(props);
+import CollectionPreview from '../../components/collection-preview/collection-preview.component';
 
-    this.state = {
-      collections: SHOP_DATA
-    };
-  }
+const ShopPage = ({ collections }) =>  (
+  <div className='shop-page'>
+    {collections.map(({ id, ...otherCollectionProps }) => (
+      <CollectionPreview key={id} {...otherCollectionProps} />
+    ))}
+  </div>
+);
 
-  render() {
-    const { collections } = this.state;
-    return (
-      <div className='shop-page'>
-        {collections.map(({ id, ...otherCollectionProps }) => (
-          <CollectionPreview key={id} {...otherCollectionProps} />
-        ))}
-      </div>
-    );
-  }
-}
+const mapStateToProps = createStructuredSelector({
+  collections: selectCollectionsForPreview
+});
 
-export default ShopPage;
+export default connect(mapStateToProps)(ShopPage);
