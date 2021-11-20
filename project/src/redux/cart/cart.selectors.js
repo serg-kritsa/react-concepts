d112a58fcdfb46da2ce37ce1849137276a5d4c06
Serg <serg@example.com>
2021-11-20 17:40:14 +0300

    REDUX: inheritance in reselect (more complicated)

--- project/src/redux/cart/cart.selectors.js
+++ project/src/redux/cart/cart.selectors.js
@@ -1,7 +1,15 @@
 import { createSelector } from 'reselect';
 
+const selectCart = state => state.cart;
+
+export const selectCartItems = createSelector(
+  [selectCart],
+  cart => cart.cartItems
+);
+
 export const selectCartItemsCount = createSelector(
-  [state => state.cart.cartItems],
+  // [state => state.cart.cartItems],
+  [selectCartItems],
   cartItems =>
     cartItems.reduce(
       (accumalatedQuantity, cartItem) =>
