a778ffc337c36cbaddb7fffde0286f71827836fb
Serg <serg@example.com>
2021-11-20 18:57:03 +0300

    added buttons for dispatching actions

--- project/src/components/checkout-item/checkout-item.component.jsx
+++ project/src/components/checkout-item/checkout-item.component.jsx
@@ -1,7 +1,9 @@
 import React from 'react';
 import { connect } from 'react-redux';
 
-import { clearItemFromCart } from '../../redux/cart/cart.actions';
+import {
+  clearItemFromCart
+} from '../../redux/cart/cart.actions';
 
 import './checkout-item.styles.scss';
 
@@ -13,7 +15,15 @@ const CheckoutItem = ({ cartItem, clearItem }) => {
         <img src={imageUrl} alt='item' />
       </div>
       <span className='name'>{name}</span>
-      <span className='quantity'>{quantity}</span>
+      <span className='quantity'>
+        <div className='arrow'>
+          &#10094;
+        </div>
+        <span className='value'>{quantity}</span>
+        <div className='arrow'>
+          &#10095;
+        </div>
+      </span>
       <span className='price'>{price}</span>
       <div className='remove-button' onClick={() => clearItem(cartItem)}>
         &#10005;
