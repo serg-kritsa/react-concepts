338905070852f9abacb11b1f94c3fd9803f06189
Serg <serg@example.com>
2021-11-21 21:59:34 +0300

    css-in-js demos

---
+++ 00-basics/src/components/css-js/demo.jsx
@@ -0,0 +1,39 @@
+import React from 'react';
+
+const textStyles = {
+  color: 'red',
+  fontSize: '24px'
+}
+
+export const Demo01 = () => (
+  <div style={textStyles}>
+    <p> TEXT </p>
+  </div>
+);
+
+// ===========================================
+
+import styled from 'styled-components';
+
+const Text = styled.div`
+  color: red;
+  font-size: 24px;
+`
+export const Demo02 = () => (
+  <div>
+    <Text> TEXT </Text>
+  </div>
+);
+// passed prop
+const Text = styled.div`
+  color: red;
+  font-size: 24px;
+  border: ${ ({isActive}) => isActive ? '1px solid black' :  '1px dotted green'}
+
+`
+export const Demo03 = () => (
+  <div>
+    <Text isActive={true}> TEXT </Text>
+    <Text isActive={false}> TEXT </Text>
+  </div>
+);
