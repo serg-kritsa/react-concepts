88aa18f78e9dc3a04b65060f87281c296f7add3f
Serg <serg@example.com>
2021-11-22 14:29:52 +0300

    code style changes

--- project/src/pages/shop/shop.component.jsx
+++ project/src/pages/shop/shop.component.jsx
@@ -1,14 +1,29 @@
 import React from 'react';
 import { Route } from 'react-router-dom';
+import { connect } from 'react-redux';
 
 import CollectionsOverview from '../../components/collections-overview/collections-overview.component';
 import CollectionPage from '../collection/collection.component';
 
-const ShopPage = ({ match }) => (
-  <div className='shop-page'>
-    <Route exact path={`${match.path}`} component={CollectionsOverview} />
-    <Route path={`${match.path}/:collectionId`} component={CollectionPage} />
-  </div>
-);
+class ShopPage extends React.Component {
+  componentDidMount() {
+  }
 
-export default ShopPage;
+  render() {
+    const { match } = this.props;
+    return (
+      <div className='shop-page'>
+        <Route exact path={`${match.path}`} component={CollectionsOverview} />
+        <Route
+          path={`${match.path}/:collectionId`}
+          component={CollectionPage}
+        />
+      </div>
+    );
+  }
+}
+
+const mapDispatchToProps = (dispatch) => ({
+});
+
+export default connect(null, mapDispatchToProps)(ShopPage);
