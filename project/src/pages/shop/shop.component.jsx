50043da774cf7f95681eea43ad51c26a6d1f2b59
Serg <serg@example.com>
2021-11-22 18:28:13 +0300

    prepare HOC with spinner

--- project/src/pages/shop/shop.component.jsx
+++ project/src/pages/shop/shop.component.jsx
@@ -6,10 +6,16 @@ import { loadShopData } from '../../firebase/firebase.utils.js';
 
 import { updateCollections } from '../../redux/shop/shop.actions';
 
+import WithSpinner from '../../components/with-spinner/with-spinner.component';
+
 import CollectionsOverview from '../../components/collections-overview/collections-overview.component';
 import CollectionPage from '../collection/collection.component';
 
+const CollectionsOverviewWithSpinner = WithSpinner(CollectionsOverview);
+const CollectionPageWithSpinner = WithSpinner(CollectionPage);
+
 class ShopPage extends React.Component {
+
   componentDidMount = async () => {
     const { updateCollections } = this.props;
 
@@ -21,19 +27,30 @@ class ShopPage extends React.Component {
     const { match } = this.props;
     return (
       <div className='shop-page'>
-        <Route exact path={`${match.path}`} component={CollectionsOverview} />
+        <Route
+          exact
+          path={`${match.path}`}
+          render={props => (
+            <CollectionsOverviewWithSpinner {...props} />
+          )}
+        />
         <Route
           path={`${match.path}/:collectionId`}
-          component={CollectionPage}
+          render={props => (
+            <CollectionPageWithSpinner {...props} />
+          )}
         />
       </div>
     );
   }
 }
 
-const mapDispatchToProps = (dispatch) => ({
-  updateCollections: (collectionsMap) =>
+const mapDispatchToProps = dispatch => ({
+  updateCollections: collectionsMap =>
     dispatch(updateCollections(collectionsMap))
 });
 
-export default connect(null, mapDispatchToProps)(ShopPage);
+export default connect(
+  null,
+  mapDispatchToProps
+)(ShopPage);
