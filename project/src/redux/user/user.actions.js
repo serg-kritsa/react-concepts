59860f0ec7bfdd20a7cb62607110ad307fb0ac88
Serg <serg@example.com>
2021-11-26 02:09:00 +0300

    SAGAS: signin w/ login/password

--- project/src/redux/user/user.actions.js
+++ project/src/redux/user/user.actions.js
@@ -14,6 +14,11 @@ export const signInFailure = error => ({
   payload: error
 });
 
+export const emailSignInStart = emailAndPassword => ({
+  type: UserActionTypes.EMAIL_SIGN_IN_START,
+  payload: emailAndPassword
+});
+
 export const checkUserSession = () => ({
   type: UserActionTypes.CHECK_USER_SESSION
 });
--- project/src/redux/user/user.sagas.js
+++ project/src/redux/user/user.sagas.js
@@ -10,8 +10,10 @@ import {
 } from './user.actions';
 
 import {
+  auth,
   getCurrentUser,
-  signInWithGooglePopup
+  signInWithGooglePopup,
+  signInWithEmailAndPasswordForm
 } from '../../firebase/firebase.utils';
 
 export function* getSnapshotFromUserAuth(userAuth) {
@@ -31,6 +33,16 @@ export function* signInWithGoogle() {
     yield put(signInFailure(error));
   }
 }
+
+export function* signInWithEmail({ payload: { email, password } }) {
+  try {
+    const { user } = yield signInWithEmailAndPasswordForm(auth, email, password);
+    yield getSnapshotFromUserAuth(user);
+  } catch (error) {
+    yield put(signInFailure(error));
+  }
+}
+
 export function* isUserAuthenticated() {
   try {
     const userAuth = yield getCurrentUser();
@@ -53,6 +65,10 @@ export function* onGoogleSignInStart() {
   yield takeLatest(UserActionTypes.GOOGLE_SIGN_IN_START, signInWithGoogle);
 }
 
+export function* onEmailSignInStart() {
+  yield takeLatest(UserActionTypes.EMAIL_SIGN_IN_START, signInWithEmail);
+}
+
 export function* onCheckUserSession() {
   yield takeLatest(UserActionTypes.CHECK_USER_SESSION, isUserAuthenticated);
 }
@@ -64,6 +80,7 @@ export function* onSignOutStart() {
 export function* userSagas() {
   yield all([
     call(onGoogleSignInStart),
+    call(onEmailSignInStart),
     call(onCheckUserSession),
     call(onSignOutStart)
   ]);
