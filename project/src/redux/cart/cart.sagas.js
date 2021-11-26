bf558e8e52c20850d37b96164eac8d442f19110d
Serg <serg@example.com>
2021-11-27 00:37:17 +0300

    SAGAS: added listener w/ empty callback. step 2

--- project/src/redux/cart/cart.sagas.js
+++ project/src/redux/cart/cart.sagas.js
@@ -1,4 +1,14 @@
+import { all, call, takeLatest } from 'redux-saga/effects';
 
+import UserActionTypes from '../user/user.types';
+
+export function* clearCartOnSignOut() {
+}
+
+export function* onSignOutSuccess() {
+  yield takeLatest(UserActionTypes.SIGN_OUT_SUCCESS, clearCartOnSignOut);
+}
 
 export function* cartSagas() {
+  yield all([call(onSignOutSuccess)]);
 }
