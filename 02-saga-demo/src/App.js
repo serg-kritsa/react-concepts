3311f2d13cf46e158de9ea80ca796910566ef6c5
Serg <serg@example.com>
2021-11-27 02:40:53 +0300

    added basic redux demo

---
+++ 02-saga-demo/src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import SagaExample from './components/sagas-example/sagas-example.component';
+
+import './App.css';
+
+const App = props => {
+  return <SagaExample />;
+};
+
+export default App;
