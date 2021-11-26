8dbd73fbb22a5df32532ec98256ceca5d502a013
Serg <serg@example.com>
2021-11-27 02:14:11 +0300

    HOOKS: added custom hook demo

--- 01-hooks-demo/src/App.js
+++ 01-hooks-demo/src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 
 import UseStateExample from './components/use-state-example/use-state-example.component';
+import CustomHookExample from './components/custom-hook-example/custom-hook-example.component';
 
 import './App.css';
 
 const App = props => {
   return <UseStateExample />;
+  // return <CustomHookExample />;
 };
 
 export default App;
---
+++ 01-hooks-demo/src/components/custom-hook-example/custom-hook-example.component.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import User from './user/user.component';
+import Post from './post/post.component';
+
+const CustomHookExample = () => {
+  return (
+    <div>
+      <Post />
+      <User />
+    </div>
+  )
+}
+
+export default CustomHookExample;
