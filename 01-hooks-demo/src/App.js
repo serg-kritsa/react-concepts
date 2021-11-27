ad1bf41ebfaf4b0bb590cc124f90c28ea20f3da2
Serg <serg@example.com>
2021-11-27 21:39:22 +0300

    added useState demos. changing state causes re-render

--- 01-hooks-demo/src/App.js
+++ 01-hooks-demo/src/App.js
@@ -2,6 +2,9 @@ import React from 'react';
 
 import UseStateExample from './components/use-state-example/use-state-example.component';
 import CustomHookExample from './components/custom-hook-example/custom-hook-example.component';
+import { UseStateExample1 } from './components/use-state-example/use-state-example-1.component';
+import { UseStateExample2 } from './components/use-state-example/use-state-example-2.component';
+
 
 import './App.css';
 
@@ -9,6 +12,8 @@ const App = props => {
   return (
     <div>
       <UseStateExample />
+      <UseStateExample1 />
+      <UseStateExample2 />
     </div>
   )
   // return <UseStateExample />;
