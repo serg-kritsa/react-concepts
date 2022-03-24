35aa22fdeae33acb0a42a16118eeb9e851741545
Serg <serg@example.com>
2022-03-24 15:19:49 +0300

    added js generator demo

--- 00-generators/generator-demo.js
+++ 00-generators/generator-demo.js
@@ -30,3 +30,41 @@ g.next(); //
 console.log(res.value, res.done); // 15 false
 g.next(); // 
 console.log(res.value, res.done); // 25 true // 25 is returned value
+// ===================================================
+// ===================================================
+function* expandSpan(xStart, xLen, yStart, yLen) {
+    const xEnd = xStart + xLen;
+    const yEnd = yStart + yLen;
+    for (let x = xStart; x < xEnd; ++x) {
+        for (let y = yStart; y < yEnd; ++y) {
+            yield {x, y};
+        }
+    }
+}
+// the same w/ usual function
+function expandSpan(xStart, xLen, yStart, yLen) {
+    const temp = [];
+    const xEnd = xStart + xLen;
+    const yEnd = yStart + yLen;
+    for (let x = xStart; x < xEnd; ++x) {
+        for (let y = yStart; y < yEnd; ++y) {
+            temp.push({x, y});
+        }
+    }
+    return temp;
+}
+// ===================================================
+// ===================================================
+function* expandRanges(ranges) {
+    for (const range of ranges) {
+        for (const item of expandRange(range)) {
+            yield item;
+        }
+    }
+}
+// yield* - Yeild delegation 
+function* expandRanges(ranges) {
+    for (const range of ranges) {
+        yield* expandRange(range);
+    }
+}
