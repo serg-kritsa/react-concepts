2de3abe7f674e6c94080bc068999445250556375
Serg <serg@example.com>
2022-04-07 23:45:32 +0300

    CONTEXT: create context object w/ default value

--- 04-react-context/src/components/Button.js
+++ 04-react-context/src/components/Button.js
@@ -1,9 +1,14 @@
 import React from 'react';
+import LanguageContext from '../contexts/LanguageContext';
 
 class Button extends React.Component {
+  static contextType = LanguageContext;
+
   render() {
+    console.log(this.context);  //  english
     return <button className="ui button primary">Submit</button>;
   }
 }
+// Button.contextType = LanguageContext; // the same as     static contextType = LanguageContext;     INSIDE   class
 
 export default Button;
---
+++ 04-react-context/src/contexts/LanguageContext.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export default React.createContext('english');
+// export default React.createContext([1, 2, 3]);
+// export default React.createContext({ color: 'red' });
