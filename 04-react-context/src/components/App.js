247eca3f2855a1690de515b21ec8eaf4f10d0673
Serg <serg@example.com>
2022-04-07 23:20:05 +0300

    added styles

--- 04-react-context/src/components/App.js
+++ 04-react-context/src/components/App.js
@@ -1,8 +1,21 @@
 import React from 'react';
 
 class App extends React.Component {
+
   render() {
-    return <div className="ui container">App</div>;
+    return (
+      <div className="ui container">
+        <div>
+          Select a language:
+          <i
+            className="flag us"
+          />
+          <i
+            className="flag nl"
+          />
+        </div>
+      </div>
+    );
   }
 }
 
