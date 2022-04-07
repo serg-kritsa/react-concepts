339e8b9af033b2f950ad00413fecf5366df1424a
Serg <serg@example.com>
2022-04-08 01:25:15 +0300

    CONTEXT: nested context 02 - consume value from 2nd provider --raw

--- 04-react-context/src/components/Button.js
+++ 04-react-context/src/components/Button.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import LanguageContext from '../contexts/LanguageContext';
+import ColorContext from '../contexts/ColorContext';
 
 class Button extends React.Component {
   renderSubmit(value) {
@@ -8,11 +9,15 @@ class Button extends React.Component {
 
   render() {
     return (
-      <button className="ui button primary">
-        <LanguageContext.Consumer>
-          {value => this.renderSubmit(value)}
-        </LanguageContext.Consumer>
-      </button>
+      <ColorContext.Consumer>
+        {color => 
+          <button className={`ui button ${color}`}>
+            <LanguageContext.Consumer>
+              {value => this.renderSubmit(value)}
+            </LanguageContext.Consumer>
+          </button>
+        }
+      </ColorContext.Consumer>
     );
   }
 }
