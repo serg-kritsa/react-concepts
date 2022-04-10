1064a24dae0c8c0934a09a8e6f71543c55bb0e53
Serg <serg@example.com>
2022-04-10 23:53:43 +0300

    fetch deleted stream details -02- get & dispatch action w/ url param

--- modal-window/client/src/components/streams/StreamDelete.js
+++ modal-window/client/src/components/streams/StreamDelete.js
@@ -1,8 +1,14 @@
 import React from 'react';
+import { connect } from 'react-redux';
 import Modal from '../Modal';
 import history from '../../history';
+import { fetchStream } from '../../actions';
 
 class StreamDelete extends React.Component {
+  componentDidMount() {
+    this.props.fetchStream(this.props.match.params.id);
+  }
+
   renderActions() {
     return (
       <React.Fragment>
@@ -27,4 +33,7 @@ class StreamDelete extends React.Component {
   }
 }
 
-export default StreamDelete;
+export default connect(
+  null,
+  { fetchStream }
+)(StreamDelete);
